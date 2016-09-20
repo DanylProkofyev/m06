a0298e1fd0b7f339427367a39514b05345e23f32
danylprokofyev <danyl01000100@gmail.com>
2016-09-20 20:01:47 +0200

    ayy lmao

--- ejemploJS/newjavascript.js
+++ ejemploJS/newjavascript.js
@@ -1,24 +1,29 @@
 /**
  * Created by usu26 on 20/09/2016.
  */
-var vidasRestantes = 3;
-var numero;
-var resultat;
-do {
-    numero = window.prompt("introdueix numero");
-    //alert("debe introducir un numero");
-    numero = parseInt(numero);
-}
-while (isNaN(numero))
-resultat = numero + vidasRestantes;
-alert("vides restantes: " + resultat);
-console.log("el numero introducido es " + numero);
+/*primera prueba*/
+// var vidasRestantes = 3;
+// var numero;
+// var resultat;
+// do {
+//     numero = window.prompt("introdueix numero");
+//     //alert("debe introducir un numero");
+//     numero = parseInt(numero);
+// }
+// while (isNaN(numero));
+// resultat = numero + vidasRestantes;
+// alert("vides restantes: " + resultat);
+// console.log("el numero introducido es " + numero);
+
+
+/*segunda prueba*/
 function obtenValorTexto() {
-    var texto = document.getElementById("texto").value;
-    document.getElementById("valor_texto").innerHTML = texto;
-    //
+    // var texto = document.getElementById("texto").value;
+    // document.getElementById("valor_texto").innerHTML = texto;
+    //\\
     // var input = document.getElementById("texto")
     // texto = input.value;
     // var div=document.getElementById("valor_texto");
     // div.innerHTML=texto;
+    document.getElementById("valor_texto").innerHTML = document.getElementById("texto").value;
 }
