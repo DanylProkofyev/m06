53f779ac6848a24491d7d9b3ed8080274ce3181e
usu26 <usu26@novaulas.stucom.com>
2016-09-20 19:27:56 +0200

    Initial commit

---
+++ ejemploJS/newjavascript.js
@@ -0,0 +1,16 @@
+/**
+ * Created by usu26 on 20/09/2016.
+ */
+var vidasRestantes = 3;
+var numero;
+var resultat;
+do {
+    numero = window.prompt("introdueix numero");
+    //alert("debe introducir un numero");
+    numero = parseInt(numero);
+}
+while (isNaN(numero))
+resultat = numero + vidasRestantes;
+alert("vides restantes: " + resultat);
+console.log("el numero introducido es " + numero);
+//ayy lmao
